5faf5599007287caeb19610b7d3cc74ad79a561b
Axizeaus <axizeaus@gmail.com>
2022-09-06 01:50:04 +0630

    image now working properly

--- src/home.js
+++ src/home.js
@@ -1,13 +1,13 @@
 import './style.css';
-// import Pizza from './pineapplePizza.png';
+import Pizza from './pineapplePizza.png';
 
 export function mainPage(){
 
     const mainDiv = document.getElementById('content');
     const h1 = document.createElement('h1');
-    // const mainImage = new Image();
+    const mainImage = new Image();
     const img = document.createElement('img');
-    // mainImage.src = Pizza;
+    mainImage.src = Pizza;
 
     h1.textContent = "Eat and Fight club";
 
