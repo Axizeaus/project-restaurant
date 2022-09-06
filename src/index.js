3b94161fd33ed2b79632a6c81213e3af1cd66dcc
Axizeaus <axizeaus@gmail.com>
2022-09-07 00:44:01 +0630

    three buttons working properly

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { loadPage } from "./load";
 import { contact } from "./contact";
 import { menu } from "./menu";
 
+import "./threeButtons.css"
+
 window.onload = () => loadPage();
 
 function home() {
   const home = document.createElement("button");
   home.textContent = "home";
+
   home.onclick = () => {clear();loadPage()};
   return home;
 }
@@ -14,6 +17,7 @@ function home() {
 function contactPage() {
   const contactBtn = document.createElement("button");
   contactBtn.textContent = 'contact';
+
   contactBtn.onclick = () => {clear();contact()};
   return contactBtn;
 }
@@ -21,6 +25,7 @@ function contactPage() {
 function menuPage() {
   const menuBtn = document.createElement('button');
   menuBtn.textContent = 'menu';
+
   menuBtn.onclick = () => {clear();menu()};
   return menuBtn;
 }
@@ -29,6 +34,7 @@ function clear(){
     const content = document.getElementById('content');
     content.innerHTML = '';
 }
+
 
 document.body.appendChild(home());
 document.body.appendChild(contactPage());
